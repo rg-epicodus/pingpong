8ea7a69a877b7411347bf9f750cb6c07e0aadffc
Richard Garrick <richg341@gmail.com>
2017-06-23 16:38:17 -0700

    revise entire project to follow instructions, add first passing spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,33 @@
 // Business Logic
 var ball = [];
-var ping = "Ping!";
-var pong = "Pong!";
-var both = "Ping Pong!";
-var miss = "Miss!!";
-var pingPong = function(volley) {
-  if ((volley % 3 === 0) && (volley % 15 !== 0)) {
-    return ping;
-  } else if ((volley % 5 === 0) && (volley % 15 !== 0)) {
-    return pong;
-  } else if ((volley % 3 === 0) && (volley % 5 === 0)) {
-    return both;
-  } else {
-    return miss;
+var volley = [];
+
+var service = (function() {
+
+  for (var i = 1; i <= n; i++) {
+    ball.push(i);
   }
-};
 
+  volley = ball.slice();
+    for (var i = 0; i <= volley.length; i++) {
+      if (volley [i] % 3 === 0) {
+        volley [i] = "Ping!"
+      }
+    }
+  $("#result").show();
 
+  volley.forEach(function(volley) {
+    $("#result h1").append("<p>" + volley + "</p>");
+  })
 
-
-
-
-
+})
 
 
 // User Interface Logic
-
-$(document).ready(function() {
-  $("form#ping-pong").submit(function(event) {
+$(document).ready(function (){
+  $("#gameForm form").submit(function (event) {
     event.preventDefault();
-    var volley = parseInt($("input#userInput").val());
-    var result = pingPong(volley);
-    $("#result").append("<p>" + result + "</p>");
-  });
+    n = $("input#number").val();
+  service();
+  })
 });
