2d6bbbe65411ed33353a54a528bb16026a204fd8
Richard Garrick <richg341@gmail.com>
2017-06-23 16:51:06 -0700

    add a third passing spec to show multiples of 15 but not 3 or 5 return PingPong

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,12 @@ var service = (function() {
 
   volley = ball.slice();
     for (var i = 0; i <= volley.length; i++) {
-      if (volley [i] % 3 === 0) {
+      if ((volley [i] % 3 === 0) && (volley [i] % 15 !== 0)){
         volley [i] = "Ping!"
-      } else if (volley [i] % 5 === 0) {
+      } else if ((volley [i] % 5 === 0) && (volley [i] % 15 !== 0)) {
         volley [i] = "Pong!"
+      } else if ((volley [i] % 3 === 0) && (volley [i] % 5 === 0)) {
+        volley [i] = "PingPong!"
       }
     }
   $("#result").show();
